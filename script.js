0c37647e6866aa55b904f2c4bf61257fcd21ace1
Hüseyn Bayramlı <huseyn.bayramli.b@asoiu.edu.az>
2024-01-03 22:41:24 +0400

    loop

---
+++ script.js
@@ -0,0 +1,131 @@
+//! Task 3.1    LOOP
+// let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2];
+// let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
+// let cem = 0;
+// let arr_1_cem = 0;
+// let arr_2_cem = 0;
+// for (i = 0; i < arr_1.length; i++) {
+//     arr_1_cem = arr_1_cem + arr_1[i]
+// }
+// for (i = 0; i < arr_2.length; i++) {
+//     arr_2_cem = arr_2_cem + arr_2[i]
+// }
+// cem = arr_1_cem + arr_2_cem
+// console.log(cem)
+
+
+//! Task 3.2
+// let arr = [2, 3, 4, 5, 6, 7, 10, 11, 12, 14, 15, 16, 17, 22, 23, 34];
+// let netice = "";
+// let n = 22
+// for (i = 0; i < arr.length; i++) {
+//     if (arr[i] % 2 == 0 && arr[i] != 0 && arr[i] <= n) {
+//         netice = netice + " " + arr[i]
+//     }
+// }
+// console.log(netice)
+
+
+//! Task 3.3
+// let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+// let arr2 = [];
+// for (i = arr.length - 1; i >= 0; i--) {
+//     arr2.push(arr[i])
+// }
+// console.log(arr2)
+
+
+//! Task 3.4
+// let arr1 = [4, 6, 7];
+// let arr2 = [8, 1, 9];
+// let arr = [];
+// for (i = 0; i < arr1.length; i++) {
+//     arr.push(arr1[i] + arr2[i])
+// }
+// console.log(arr)
+
+
+//! Task 3.5
+// let str = "don’t know why";
+// for (i = 0; i < str.length; i++) {
+//     if (str[i] == "y") {
+//         console.log("true")
+//         break;
+//     }
+// }
+
+
+//! Task 3.6
+// let num = 4;
+// let factor = 1;
+// for (i = 1; i <= num; i++) {
+//     factor = factor * i
+// }
+// console.log(factor)
+
+
+//! Task 3.7
+// let str = prompt('string palindrome?')
+// let str2 = "";
+// for (i = str.length - 1; i >= 0; i--) {
+//     str2 = str2 + str[i]
+// }
+// if (str == str2) {
+//     console.log(" Yes ")
+// } else {
+//     console.log(" No ")
+// }
+
+
+//! Task 3.8
+// let nums=[];
+// for(i=1;i<=45;i++){
+//     nums.push(i)
+//     if(i%3==0 && i%5==0){
+//         console.log(`${i}- FizzBuzz`)
+//     }else if(i%3==0){
+//         console.log(`${i}-Fizz`)
+//     }else if(i%5==0){
+//         console.log(`${i}-Buzz`)
+//     }
+// }
+
+
+
+
+//! Task 3.9
+// const thisIsAnArray = ["element1", "element2", "element3", "element4"];
+// for (i = 0; i < thisIsAnArray.length; i++) {
+//     console.log(thisIsAnArray[i])
+// }
+
+
+//! Task 3.10
+// let num1 = prompt('num1');
+// let num2 = prompt('num2');
+// if (num1 > num2) {
+//     for (i = num1; i >= num2; i--) {
+//         if (i % 2 == 0) {
+//             console.log(i)
+//         }
+//     }
+// } else {
+//     for (i = num2; i >= num1; i--) {
+//         if (i % 2 == 0) {
+//             console.log(i)
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
